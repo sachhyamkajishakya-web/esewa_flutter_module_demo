9ff9782c4cdad81f2bbf5b131af05cce3fb0c924
Sachhyam Kaji Shakya <sachhyamkaji.shakya@gmail.com>
2026-03-05 17:18:30 +0545

    add custom spacing attributes

---
+++ lib/core/constants/spacing.dart
@@ -0,0 +1,20 @@
+class AppSpacing {
+  static const double xs = 4;
+  static const double sm = 8;
+  static const double md = 16;
+  static const double lg = 24;
+  static const double xl = 32;
+  static const double xxl = 48;
+}
+
+class AppRadius {
+  static const double sm = 4;
+  static const double md = 8;
+  static const double lg = 16;
+}
+
+class AppIconSize {
+  static const double sm = 16;
+  static const double md = 24;
+  static const double lg = 32;
+}
